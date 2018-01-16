0297c39bda38534dd5f833b024f95b11fe191a61
mona <mona.calibani@hetic.net>
2018-01-16 17:19:28 +0100

    tabbar + card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,20 @@
-/**
- * Created by isabellemelchiori on 16/01/2018.
- */
+import React, { Component } from 'react';
+import { Page } from 'react-onsenui'
+
+export default class Card extends Component {
+
+    render() {
+        return (
+            <Page>
+                <article className="card__article">
+                    <img src={this.props.placeImg} className="card__img"/>
+                    <div className="card__article--infos">
+                        <h2>{this.props.placeName}</h2>
+                        <p>{this.props.placeLevel}</p>
+                        <p>{this.props.placeDescription}</p>
+                    </div>
+                </article>
+            </Page>
+        )
+    }
+}
