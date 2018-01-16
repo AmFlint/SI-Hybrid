f576d7d0c89f61c0b4be6a7f9a06d088ab331fd7
Nico <nicolas.leroy@hetic.net>
2018-01-16 10:16:06 +0100

    Set a default action/reducer & set regeneratorRunTime

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './test.scss'
 import {
   Navigator
 } from 'react-onsenui';
+import {bindActionCreators} from "redux";
+import {hello} from "../actions/homeActions";
+import {connect} from "react-redux";
 
 const renderPage = (route, navigator) => (
   <route.component key={route.key} navigator={navigator} />
 );
 
-const App = () => (
-  // <Navigator
-  //   renderPage={renderPage}
-  //   initialRoute={{component: MainPage, key: 'MAIN_PAGE'}}
-  //>
-  <h1>Hey ee</h1>
-);
 
-export default App;
+class App extends Component {
+
+  componentDidMount() {
+    const { hello } = this.props;
+    hello();
+  }
+
+  render() {
+      const {homeMessage} = this.props;
+      // <Navigator
+      //   renderPage={renderPage}
+      //   initialRoute={{component: MainPage, key: 'MAIN_PAGE'}}
+      //>
+      return (
+          <h1>{ homeMessage }</h1>
+      )
+  };
+}
+
+
+
+function mapStateToProps(state) {
+    return {
+      homeMessage : state.home.message
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        hello
+    }, dispatch
+)}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
