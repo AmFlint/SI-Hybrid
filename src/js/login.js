23973d4cc24dfff85cb62613e28bc036e9dcdad5
Nico <nicolas.leroy@hetic.net>
2018-01-17 17:20:10 +0100

    Render home page

--- src/js/login.js
+++ src/js/login.js
@@ -30,7 +30,7 @@ function login(e) {
         .then((response) => {
             if (response.status === 200) {
                 saveUserToken(response.data);
-                redirectTo('index')
+                redirectTo('home')
             }
         })
         .catch((err) => console.log(err.message))
