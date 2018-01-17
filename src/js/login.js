74c7931a94b2a3ba26735706b111dae53be84b94
Nico <nicolas.leroy@hetic.net>
2018-01-17 16:08:06 +0100

    Create login action

--- src/js/login.js
+++ src/js/login.js
@@ -1,1 +1,49 @@
 import '../styles/main.scss'
+import axios from 'axios';
+import { URL } from './config/config'
+const yay = 'yay'
+
+const form = document.querySelector('.submit__box');
+
+form.addEventListener( 'submit' , login )
+
+
+function login(e) {
+    e.preventDefault();
+    const userName = this.querySelector('#userName');
+    const password = this.querySelector('#password');
+    if (!inputIsEmpty()) {
+        axios({
+            url : URL + '/login',
+            method : 'POST',
+            data : {
+                username : userName.value,
+                password : password.value
+            },
+            headers : {
+                "Access-Control-Allow-Origin":  "*",
+                "Access-Control-Allow-Methods": "POST",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization"
+            }
+        })
+        .then((response) => response.status === 200 ? saveUserLog(response.data) : console.log('error'))
+        .catch((err) => console.log(err.message))
+    }
+}
+
+function inputIsEmpty() {
+    if (typeof document !== "undefined") {
+        var userName = window.document.querySelector('#userName');
+        var password = window.document.querySelector('#password');
+
+        return !userName.value.trim() && !password.value.trim()
+    }
+}
+
+function saveUserLog(value) {
+    window.localStorage.setItem('logAuth', JSON.stringify(value.token))
+}
+
+
+
+
