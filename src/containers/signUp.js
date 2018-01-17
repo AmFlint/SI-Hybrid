bdb6cd8de64f56a752627ff7901d27ea0c6c5d43
lacaux-gerome <gerome.lacaux@hetic.net>
2018-01-17 10:47:27 +0100

    feature de base

--- src/containers/signUp.js
+++ src/containers/signUp.js
@@ -10,20 +10,27 @@ class signUp extends Component {
         password: "",
         numberMinPsw: 6,
         errorMessage: "",
-        buttonTxt: "s'inscrire"
+        buttonTxt: "Entrer dans le groupe",
+        mail: ""
     };
 
-    handleChange = (e) => {
+    handleChange(e) {
         const name = e.target.name;
         const value = e.target.value;
         //check if the input changed is password
+        this.checkPsw(name, value);
+    };
+
+    checkPsw(name, value) {
         if (name === "password") {
+            //check lenght of entry in password field
             if (value.length >= this.state.numberMinPsw) {
                 this.setState({
                     [name]: value,
                     errorMessage: ""
                 })
             }
+            //errors too much char
             else {
                 this.setState({
                     errorMessage: "Mot de passe de au moins " + this.state.numberMinPsw + " charactères",
@@ -32,6 +39,7 @@ class signUp extends Component {
         }
         if (name !== "password") {
             this.setState({
+                [name]: value,
                 errorMessage: ""
             })
         }
@@ -39,41 +47,68 @@ class signUp extends Component {
 
     render() {
         return (
-            <form className="submit">
-                <h2 className="submit__title">S'inscrire</h2>
-                <div>
-                    <Input
-                        float
-                        type="text"
-                        modifier="material"
-                        name="username"
-                        placeholder="Username"
-                        onChange={this.handleChange}
-                        className="submit__input"
-                    />
-                </div>
-                <div>
-                    <Input
-                        float
-                        type="password"
-                        modifier="material"
-                        name="password"
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                        className="submit__input"
-                    />
-                </div>
-                <p className="submit__error-message">{this.state.errorMessage}</p>
-                <div>
-                    <Button
-                        float
-                        modifier="material large"
-                        className="submit__button"
-                    >
-                        {this.state.buttonTxt}
-                    </Button>
-                </div>
-            </form>
+            <div className="submit">
+                <h2 className="submit__title">Vous nous <span>rejoignez ?</span></h2>
+                <form className="submit__box submit__box--shadow">
+                    <div className="submit__input">
+                        <Input
+                            float
+                            type="mail"
+                            modifier="material"
+                            name="mail"
+                            placeholder="Mail"
+                            onChange={(e) => this.handleChange(e)}
+                            className="submit__input__content"
+                        />
+                    </div>
+                    <div className="submit__input">
+                        <Input
+                            float
+                            type="text"
+                            modifier="material"
+                            name="username"
+                            placeholder="Username"
+                            onChange={(e) => this.handleChange(e)}
+                            className="submit__input__content"
+                        />
+                    </div>
+                    <div className="submit__input">
+                        <Input
+                            float
+                            type="password"
+                            modifier="material"
+                            name="password"
+                            placeholder="Password"
+                            onChange={(e) => this.handleChange(e)}
+                            className="submit__input__content"
+                        />
+                    </div>
+                    <div className="submit__input">
+                        <Input
+                            float
+                            type="password"
+                            modifier="material"
+                            name="password-verif"
+                            placeholder="Password verif"
+                            onChange={(e) => this.handleChange(e)}
+                            className="submit__input__content"
+                        />
+                    </div>
+                    <p className="submit__box__error-message">{this.state.errorMessage}</p>
+                    <div className="submit__button">
+                        <Button
+                            float
+                            modifier="material large"
+                        >
+                            {this.state.buttonTxt}
+                        </Button>
+                    </div>
+                    <div className="submit__box__mdp">
+                        <a href="">Mot de passe oublié ?</a>
+                    </div>
+                </form>
+                <a className="submit__box__inscription" href="">Pas encore inscrit ? <span>Sign Up !</span></a>
+            </div>
         )
     };
 }
