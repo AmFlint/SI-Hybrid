38733acf2b562b3fe7bd425343a674c3ee61c4f0
Nicolas LEROY <yoreln@mini-de-nicolas.home>
2018-01-18 01:29:55 +0100

    Create method for add posts

--- src/js/add-card.js
+++ src/js/add-card.js
@@ -1,4 +1,8 @@
 import '../styles/main.scss'
+import axios from "axios";
+import {URL} from "./config/config";
+import {redirectTo} from "./helpers/redirect";
+import {getUserToken} from "./helpers/auth";
 
 let stars = document.querySelectorAll('.add__card--rate > i');
 
@@ -11,4 +15,56 @@ for (let i=0 ; i<stars.length; i++ ) {
             stars[j].classList.add('active');
         }
     })
+}
+
+const form = document.querySelector('.submit__box');
+
+form.addEventListener('submit', addCard);
+
+function getLevel() {
+    const level = document.querySelectorAll('.icon-star_rate_18px.active');
+    return level.length
+}
+
+function addCard(e) {
+    e.preventDefault();
+    const placeName = window.document.querySelector('#placeName');
+    const placeDrescription = window.document.querySelector('#placeDrescription');
+    const placeAddress = window.document.querySelector('#placeAddress');
+    const placeLevel = getLevel();
+    if (!inputIsEmpty()) {
+        axios({
+            url : URL + '/posts',
+            method : 'POST',
+            data : {
+                title : placeName.value,
+                location : placeAddress.value,
+                difficulty : placeLevel,
+                content : placeDrescription.value,
+            },
+            headers : {
+                "Access-Control-Allow-Origin":  "*",
+                "Access-Control-Allow-Methods": "POST",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "X-Access-Token" : getUserToken()
+            }
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    redirectTo('home');
+                }
+            })
+            .catch((err) => console.log(err.message))
+    }
+}
+
+function inputIsEmpty() {
+    if (typeof document !== "undefined") {
+        const placeName = window.document.querySelector('#placeName');
+        const placeDrescription = window.document.querySelector('#placeDrescription');
+        const placeAddress = window.document.querySelector('#placeAddress');
+        const placeLevel = getLevel();
+
+        return !placeName.value.trim() && !placeDrescription.value.trim() && !placeAddress.value.trim() && !placeLevel.length > 0
+    }
 }
